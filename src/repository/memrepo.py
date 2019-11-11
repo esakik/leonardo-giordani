cd7de6d4f7cde21698f1db64b6a99cfdfa672a4a
esaki01 <social0430@gmail.com>
2019-11-11 23:22:11 +0900

    Add filter to memrepo

--- src/repository/memrepo.py
+++ src/repository/memrepo.py
@@ -1,9 +1,27 @@
-from src.domain import room as r
+from src.domain import room
 
 
 class MemRepo:
     def __init__(self, data):
         self.data = data
 
-    def list(self):
-        return [r.Room.from_dict(i) for i in self.data]
+    def list(self, filters=None):
+
+        result = [room.Room.from_dict(i) for i in self.data]
+
+        if filters is None:
+            return result
+
+        if 'code__eq' in filters:
+            result = [r for r in result if r.code == filters['code__eq']]
+
+        if 'price__eq' in filters:
+            result = [r for r in result if r.price == filters['price__eq']]
+
+        if 'price__lt' in filters:
+            result = [r for r in result if r.price < filters['price__lt']]
+
+        if 'price__gt' in filters:
+            result = [r for r in result if r.price > filters['price__gt']]
+
+        return result
